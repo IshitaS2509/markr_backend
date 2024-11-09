fb79f4f9d312d4fa4d4a2a2d5b829721b3191b54
IshitaS2509 <117073024+IshitaS2509@users.noreply.github.com>
2024-11-09 23:21:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const gpaCalculatorRoutes = require('./routes/gpaCalculator');
 const aboutRoutes = require('./routes/about');
+const contactRoutes = require('./routes/contactRoutes');
 
 
 const app = express();
@@ -12,11 +13,20 @@ app.use(express.json());
 app.use('/api/gpa', gpaCalculatorRoutes);
 app.use('/api/about', aboutRoutes);
 
+
+
 mongoose.connect('mongodb://127.0.0.1:27017/mern_website', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
+
+mongoose.connect('mongodb://localhost:27017/contactdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use('/api/contact', contactRoutes);
 
 
 const PORT = process.env.PORT || 5001;
